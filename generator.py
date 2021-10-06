d0acdd0da14a28500d88ce90e2a8da0ddc4c99cd
Andrey-Pidgorodetsky <andrey2007@ukr.net>
2021-10-06 15:00:40 +0300

    New Generator

--- generator.py
+++ generator.py
@@ -3,14 +3,37 @@
 # print(my_function(4))
 
 
-def couner_up_to(x):
-    count=1
-    while count <=x:
-        yield count
-        count+=2
+# def couner_up_to(x):
+#     count=1
+#     while count <=x:
+#         yield count
+#         count+=1
+# for i in couner_up_to(10):
+#     print(i)
 
+# def crate_patterns():
+#     max_patterns_number=100
+#     patterns=('First pattern', 'Second pattern', 'Third pattern', 'Forth pattern')
+#     i=0
+#     result_list=[]
+#     while len(result_list)<max_patterns_number:
+#         if i>= len(patterns):
+#             i=0
+#         result_list.append(patterns[i])
+#         i+=1
+#     return result_list
+#
+# print(crate_patterns())
+def get_current_pattern():
+    patterns=('First pattern', 'Second pattern', 'Third pattern', 'Forth pattern')
+    i=0
+    while True:
+        if i>=len(patterns):
+            break
+        yield patterns[i]
+        i+=1
 
+current_pattern=get_current_pattern()
 
-
-for i in couner_up_to(10):
+for i in current_pattern:
     print(i)
---
+++ time_generator.py
@@ -0,0 +1,12 @@
+import time
+
+list_start_time=time.time()
+print(sum([number for number in range(100000000)]))
+list_processing_time=time.time()-list_start_time
+
+generator_start_time=time.time()
+print(sum(number for number in range(100000000)))
+generator_processing_time=time.time()-list_start_time
+
+print(f'List processing {list_processing_time}')
+print(f'Generator processing {generator_processing_time}')
